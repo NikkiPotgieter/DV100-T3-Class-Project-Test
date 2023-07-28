411acd83c288f5ef054b515840b74c1445592fa4
NikkiPotgieter <125267113+NikkiPotgieter@users.noreply.github.com>
2023-07-28 14:03:38 +0200

    Accordion and Wishlist
    Add photos to accordion. Add table to Wishlist.

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,10 @@ $(document).ready(function(){
 
     // Hide all description text from the plant cards
     $("#descriptionText").hide();
+    $("#descriptionText2").hide();
+    $("#descriptionText3").hide();
+    $("#descriptionText4").hide();
+    $("#descriptionText5").hide();
 
   }); 
 
@@ -23,8 +27,16 @@ $(document).ready(function(){
     // Toggle the price & description text
     $("#priceText").toggle();
     $("#descriptionText").toggle();
+    $("#descriptionText2").toggle();
+    $("#descriptionText3").toggle();
+    $("#descriptionText4").toggle();
+    $("#descriptionText5").toggle();
 
     // Resize the image to fit the additional content
     $(".card-img-top").toggleClass("small");
 
+  });
+
+  $(document).on('click', '.remove-btn', function() {
+    $(this).closest('tr').remove();
   });
